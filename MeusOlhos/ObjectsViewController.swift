f658b3060c36ef430b041d4d7a30c40a0095f8bf
Lucas <lucasbrito_9@hotmail.com>
2020-01-12 14:08:30 -0300

    Created the project, prepared the views and added the .mlmodel.

---
+++ MeusOlhos/ObjectsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ObjectsViewController.swift
+//  MeusOlhos
+//
+//  Created by Lucas Santana Brito on 12/01/20.
+//  Copyright © 2020 lsb.br. All rights reserved.
+//
+
+import UIKit
+
+class ObjectsViewController: UIViewController {
+    
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var identifierLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func analyse(_ sender: UIButton) {
+    }
+    
+}
