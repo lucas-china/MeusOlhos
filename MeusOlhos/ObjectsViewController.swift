b0743d43ea72ce2c0e492514c1c4f4e128e55865
Lucas <lucasbrito_9@hotmail.com>
2020-01-13 23:59:04 -0300

    Objects with CoreML and Vision detected.

--- MeusOlhos/ObjectsViewController.swift
+++ MeusOlhos/ObjectsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
 class ObjectsViewController: UIViewController {
     
@@ -14,13 +16,55 @@ class ObjectsViewController: UIViewController {
     @IBOutlet weak var identifierLabel: UILabel!
     @IBOutlet weak var confidenceLabel: UILabel!
     
+    lazy var captureManager: CaptureManager = {
+       let captureManager = CaptureManager()
+        captureManager.videoBufferDelegate = self
+        return captureManager
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        identifierLabel.text = ""
+        confidenceLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let previewLayer = captureManager.startCaptureCamera() else { return }
+        previewLayer.frame = cameraView.bounds
+        cameraView.layer.addSublayer(previewLayer)
     }
     
     @IBAction func analyse(_ sender: UIButton) {
     }
     
+}
+
+extension ObjectsViewController: AVCaptureVideoDataOutputSampleBufferDelegate  {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            for i in 0...5 {
+                print(results[i].identifier, results[i].confidence)
+            }
+            print("===============================")
+            
+            guard let firstObservation = results.first else { return }
+            DispatchQueue.main.async {
+                self.identifierLabel.text = firstObservation.identifier
+                let confidence = round(firstObservation.confidence * 1000) / 10
+                self.confidenceLabel.text = String(format: "%.0f", confidence) + "%"
+            }
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
+        
+    }
 }
